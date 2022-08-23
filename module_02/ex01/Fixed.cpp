fd4eba151610da8f775a9b50a4c6e8ae35e87a29
Youssef Anab <yanab@e1r11p12.1337.ma>
2022-08-23 05:22:17 +0100

    🚣 Update

--- module_02/ex01/Fixed.cpp
+++ module_02/ex01/Fixed.cpp
@@ -1,9 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yanab <yanab@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/23 04:25:43 by yanab             #+#    #+#             */
+/*   Updated: 2022/08/23 05:13:48 by yanab            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Fixed.hpp"
 
 Fixed::Fixed(void)
 {
 	std::cout << "Default constructor called" << std::endl;
 	Fixed::num_value = 0;
+}
+
+Fixed::Fixed(const int value)
+{
+	std::cout << "Int constructor called" << std::endl;
+	Fixed::num_value = value;
+	Fixed::num_fract = 8;
+}
+
+Fixed::Fixed(const float value)
+{
+	std::cout << "Float constructor called" << std::endl;
+	Fixed::num_value = roundf(value);
+	Fixed::num_fract = value % 1;
 }
 
 Fixed::~Fixed(void)
@@ -33,4 +59,23 @@ int Fixed::getRawBits(void) const
 void Fixed::setRawBits(int const raw)
 {
 	Fixed::num_value = raw;
+}
+
+float	Fixed::toFloat(void) const
+{
+	// std::cout << "toFloat" << std::endl;
+	return (1.1);
+}
+
+int		Fixed::toInt(void) const
+{
+	// std::cout << "toInt" << std::endl;
+	return (this->num_value);
+}
+
+std::ostream	&operator<<(std::ostream &output, const Fixed &obj)
+{
+	(void)obj;
+	output << "<< operator";
+	return (output);
 }
--- module_02/ex01/Fixed.hpp
+++ module_02/ex01/Fixed.hpp
@@ -1,7 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yanab <yanab@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/23 04:25:42 by yanab             #+#    #+#             */
+/*   Updated: 2022/08/23 05:05:54 by yanab            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FIXED_H
 # define FIXED_H
 
 # include <iostream>
+# include <cmath>
 
 class Fixed
 {
@@ -11,11 +24,17 @@ class Fixed
 
 	public:
 		Fixed(void);
+		Fixed(const int value);
+		Fixed(const float value);
 		~Fixed(void);
 		Fixed(const Fixed &obj);
 		Fixed	&operator=(const Fixed &obj);
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
+
+		friend std::ostream	&operator<<(std::ostream &output, const Fixed &obj);
 };
 
 #endif
