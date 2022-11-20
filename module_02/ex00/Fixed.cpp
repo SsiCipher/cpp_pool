ec26e7dc808280e7528e5a4ca605b453640902ab
SsiCipher <SsiCipher@github.com>
2022-11-20 08:52:02 +0100

    😑 ADDED MO2/EX01

--- module_02/ex00/Fixed.cpp
+++ module_02/ex00/Fixed.cpp
@@ -1,21 +1,9 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Fixed.cpp                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yanab <yanab@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/23 04:25:59 by yanab             #+#    #+#             */
-/*   Updated: 2022/08/23 04:26:00 by yanab            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "Fixed.hpp"
 
 Fixed::Fixed(void)
 {
 	std::cout << "Default constructor called" << std::endl;
-	Fixed::num_value = 0;
+	Fixed::_num_value = 0;
 }
 
 Fixed::~Fixed(void)
@@ -32,17 +20,17 @@ Fixed::Fixed(const Fixed &obj)
 Fixed &Fixed::operator=(const Fixed &obj)
 {
 	std::cout << "Copy assignment operator called" << std::endl;
-	Fixed::num_value = obj.getRawBits();
+	Fixed::_num_value = obj.getRawBits();
 	return (*this);
 }
 
 int Fixed::getRawBits(void) const
 {
 	std::cout << "getRawBits member function called" << std::endl;
-	return (Fixed::num_value);
+	return (Fixed::_num_value);
 }
 
 void Fixed::setRawBits(int const raw)
 {
-	Fixed::num_value = raw;
+	Fixed::_num_value = raw;
 }
--- module_02/ex00/Fixed.hpp
+++ module_02/ex00/Fixed.hpp
@@ -1,31 +1,20 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Fixed.hpp                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yanab <yanab@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/23 04:25:55 by yanab             #+#    #+#             */
-/*   Updated: 2022/08/23 04:25:56 by yanab            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#ifndef FIXED_HPP
+#define FIXED_HPP
 
-#ifndef FIXED_H
-# define FIXED_H
-
-# include <iostream>
+#include <iostream>
 
 class Fixed
 {
 	private:
-		int					num_value;
-		static const int	num_fract = 8;
+		int _num_value;
+		static const int _num_fract = 8;
 
 	public:
 		Fixed(void);
 		~Fixed(void);
 		Fixed(const Fixed &obj);
-		Fixed	&operator=(const Fixed &obj);
+		Fixed &operator=(const Fixed &obj);
+
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
 };
