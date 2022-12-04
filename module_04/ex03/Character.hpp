8d3a88c7d32bb887b68d85be626f7bb4afc68ae6
Youssef Anab <yanab@e2r9p8.1337.ma>
2022-12-04 15:24:29 +0100

    💿 ADDED: M04/EX03

---
+++ module_04/ex03/Character.hpp
@@ -0,0 +1,27 @@
+#ifndef CHARACTER_HPP
+#define CHARACTER_HPP
+
+#include <iostream>
+#include "ICharacter.hpp"
+#include "AMateria.hpp"
+
+class Character: public ICharacter
+{
+	private:
+		std::string		_name;
+		AMateria		*_inventory;
+		unsigned int	_inventory_size;
+
+	public:
+		Character(void);
+		~Character(void);
+		Character(const Character &obj);
+		Character &operator=(const Character &obj);
+
+		std::string const	&getName() const;
+		void				equip(AMateria* m);
+		void				unequip(int idx);
+		void				use(int idx, ICharacter& target);
+};
+
+#endif
