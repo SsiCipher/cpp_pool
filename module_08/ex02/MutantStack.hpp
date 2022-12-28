e4f755abecbd3808be6d19573744a6f89e790ea7
SsiCipher <SsiCipher@github.com>
2022-12-28 11:04:53 +0100

    🥪 FINISHED: M08/EX02

--- module_08/ex02/MutantStack.hpp
+++ module_08/ex02/MutantStack.hpp
@@ -8,15 +8,18 @@ template <typename T>
 class MutantStack: public std::stack<T>
 {
 	public:
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+
 		MutantStack() {};
 		MutantStack(const MutantStack& obj) { *this = obj; };
 		MutantStack& operator=(const MutantStack& obj) { (void)obj; return (*this); };
 		~MutantStack() {};
 
-		typename std::stack<T>::container_type::iterator begin() { return (this->c.begin()); }
-		typename std::stack<T>::container_type::const_iterator begin() { return (this->c.begin()); }
-		typename std::stack<T>::container_type::iterator end() { return (this->c.end()); }
-		typename std::stack<T>::container_type::const_iterator end() { return (this->c.end()); }
+		iterator begin() { return (this->c.begin()); }
+		iterator end() { return (this->c.end()); }
+		reverse_iterator rbegin() { return (this->c.rbegin()); }
+		reverse_iterator rend() { return (this->c.rend()); }
 };
 
 #endif
--- module_08/ex02/main.cpp
+++ module_08/ex02/main.cpp
@@ -1,6 +1,39 @@
-#include "easyfind.hpp"
+#include "MutantStack.hpp"
 
 int main()
 {
+	MutantStack<int>	mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << "The top is : " << mstack.top() << std::endl;
+	
+	mstack.pop();
+
+	std::cout << "the stack has : " << mstack.size() << " elements." << std::endl;
+
+	mstack.push(3);
+	mstack.push(90);
+	mstack.push(737);
+	mstack.push(0);
+	
+	MutantStack<int>::iterator it = mstack.begin();
+	// MutantStack<int>::reverse_iterator ite = mstack.rend();
+
+	++it;
+	--it;
+
+	for (; it != mstack.end(); it++)
+		std::cout << *it << std::endl;
+
+	// while (it != ite)
+	// {
+	// 	std::cout << *it << std::endl;
+	// 	++it;
+	// }
+
+	std::stack<int> s(mstack);
+
 	return (0);
 }
