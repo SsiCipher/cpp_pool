82b56b3845d6c528c1b994a7df2ca521a7d4084e
Youssef Anab <yanab@e2r9p2.1337.ma>
2023-03-17 06:14:06 +0100

    CPP09/EX00

--- module_09/ex00/main.cpp
+++ module_09/ex00/main.cpp
@@ -1,9 +1,22 @@
 #include "BitcoinExchange.hpp"
 
-bool	check_date(std::string &date_str)
+bool is_date_valid(std::string date)
 {
-	std::vector 
-	date_str.
+	std::string::size_type start = 0;
+	std::string::size_type pos = -1;
+	std::vector<int> dateParts;
+
+	while ((pos = date.find('-', pos + 1)))
+	{
+		dateParts.push_back(atoi(date.substr(start, pos - start).c_str()));
+		start = pos + 1;
+		if (pos == std::string::npos)
+			break;
+	}
+
+	if (dateParts.size() != 3 || !(dateParts[0] > 0 && dateParts[0] <= 2023) || !(dateParts[1] > 0 && dateParts[1] <= 12) || !(dateParts[2] > 0 && dateParts[2] <= 31))
+		return (false);
+	return (true);
 }
 
 int main(int argc, char *argv[])
