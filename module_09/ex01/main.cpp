cd989995f6b298c93ce25a5d70e86f9d4a36d92d
Youssef Anab <yanab@e2r13p9.1337.ma>
2023-03-18 21:22:03 +0100

    😜 FINISHED: M09/EX01

--- module_09/ex01/main.cpp
+++ module_09/ex01/main.cpp
@@ -17,34 +17,68 @@ vertor_s split_arg(std::string &str)
 
 int	main(int argc, char const *argv[])
 {
-	size_t			start = 0;
 	std::string		expr;
 	std::stack<int>	stack;
+	int				a, b;
+	size_t			pos, start;
 
 	if (argc != 2)
 		std::cerr << "Error: Invalid arguments" << std::endl;
 	else
 	{
+		pos = -1;
+		start = 0;
 		expr = argv[1];
-		for (int i = 0; expr[i]; i++)
+		while ((pos = expr.find(' ', pos + 1)))
 		{
-			for (; expr[i] == ' '; i++);
-			if (expr[i] == '+' || expr[i] == '-' || expr[i] == '*' || expr[i] == '/')
+			if (expr.substr(start, pos - start) == "+")
 			{
-				std::cout << "==========" << std::endl;
-				std::cout << stack.top() << std::endl;
+				a = stack.top();
 				stack.pop();
-				std::cout << stack.top() << std::endl;
+				b = stack.top();
 				stack.pop();
-				std::cout << "==========" << std::endl;
+				stack.push(b + a);
+				std::cout << b << " + " << a << " = " << b + a << std::endl;
 			}
-			else if (expr[i] == ' ')
+			else if (expr.substr(start, pos - start) == "-")
 			{
-				stack.push(atoi(expr.substr(start, i).c_str()));
-				for (; expr[i] == ' '; i++);
-				start = i + 1;
+				a = stack.top();
+				stack.pop();
+				b = stack.top();
+				stack.pop();
+				stack.push(b - a);
+				std::cout << b << " - " << a << " = " << b - a << std::endl;
 			}
+			else if (expr.substr(start, pos - start) == "*")
+			{
+				a = stack.top();
+				stack.pop();
+				b = stack.top();
+				stack.pop();
+				stack.push(b * a);
+				std::cout << b << " * " << a << " = " << b * a << std::endl;
+			}
+			else if (expr.substr(start, pos - start) == "/")
+			{
+				a = stack.top();
+				stack.pop();
+				b = stack.top();
+				stack.pop();
+				stack.push(b / a);
+				std::cout << b << " / " << a << " = " << b / a << std::endl;
+			}
+			else
+			{
+				stack.push(atoi(expr.substr(start, pos - start).c_str()));
+				std::cout << "[" << atoi(expr.substr(start, pos - start).c_str()) << "]" << std::endl;
+			}
+			start = pos + 1;
+			if (pos == std::string::npos)
+				break;
 		}
 	}
+
+	std::cout << "Stack size: " << stack.size() << "  |  result: " << stack.top() << std::endl;
+
 	return (0);
 }
