cee1db01f3d8417bedf5af04c871d6c75ce7debd
Youssef Anab <yanab@e2r10p12.1337.ma>
2023-03-29 07:22:07 +0000

    🍰 UPDATE : Refactored M09/EX01

---
+++ module_09/ex01_redo/RPN.hpp
@@ -0,0 +1,18 @@
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include <iostream>
+#include <stack>
+
+class RPN
+{
+	private:
+
+	public:
+		RPN(void);
+		~RPN(void);
+		RPN(const RPN &obj);
+		RPN &operator=(const RPN &obj);
+};
+
+#endif
