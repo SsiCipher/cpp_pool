317f39e17a252ecd83206d9cfd1b66d7284801a9
Youssef Anab <yanab@e2r9p8.1337.ma>
2023-04-13 00:33:23 +0000

    🎱 FIXED: M09/EX1

--- module_09/ex01/RPN.cpp
+++ module_09/ex01/RPN.cpp
@@ -2,23 +2,103 @@
 
 RPN::RPN(void)
 {
-	std::cout << "[RPN] Default constructor has been called!" << std::endl;
 }
 
 RPN::~RPN()
 {
-	std::cout << "[RPN] Destructor has been called!" << std::endl;
 }
 
 RPN::RPN(const RPN &obj)
 {
-	std::cout << "[RPN] Copy constructor has been called!" << std::endl;
 	*this = obj;
 }
 
 RPN &RPN::operator=(const RPN &obj)
 {
-	std::cout << "[RPN] Copy assignment operator has been called!" << std::endl;
 	(void)obj;
 	return (*this);
+}
+
+void	RPN::_trim_str(std::string &str)
+{
+	size_t s, e;
+
+	for (s = 0; str[s] == ' ' || str[s] == '\t'; s++);
+	for (e = str.size() - 1; str[e] == ' ' || str[e] == '\t'; e--);
+	str.erase(e + 1);
+	str.erase(0, s);
+}
+
+bool	RPN::_is_str_digit(std::string str)
+{
+	for (size_t i = 0; i < str.size(); i++)
+		if (!isdigit(str[i]))
+			return (false);
+	return (true);
+}
+
+void	RPN::_do_operation(t_stack &stack, int a, int b, char op)
+{
+	switch (op)
+	{
+		case '+':
+			stack.push(a + b);
+			break;
+		case '-':
+			stack.push(a - b);
+			break;
+		case '*':
+			stack.push(a * b);
+			break;
+		case '/':
+			stack.push(a / b);
+			break;
+		default:
+			break;
+	}
+}
+
+bool	RPN::_is_operation(std::string chr)
+{
+	return (chr.size() == 1 && (chr[0] == '+' || chr[0] == '-' || chr[0] == '*' || chr[0] == '/'));
+}
+
+void	RPN::eval(std::string expr)
+{
+	int		a, b;
+	size_t	pos = 0, start = 0;
+
+	_trim_str(expr);
+
+	while ((pos = expr.find(' ', start)) && start != expr.size())
+	{
+		if (!_is_operation(expr.substr(start, pos - start)))
+		{
+			if (!_is_str_digit(expr.substr(start, pos - start)))
+				throw std::invalid_argument("Error: non-numeric arguments");
+			_stack.push(atoi(expr.substr(start, pos - start).c_str()));
+		}
+		else
+		{
+			if (_stack.size() != 2)
+				throw std::invalid_argument("Error: too many operands");
+			a = _stack.top();
+			_stack.pop();
+			b = _stack.top();
+			_stack.pop();
+			_do_operation(_stack, a, b, expr.substr(start, pos - start)[0]);
+		}
+		start = pos + 1;
+		while (expr[start] == ' ')
+			start++;
+		if (pos == std::string::npos)
+			break;
+	}
+}
+
+void	RPN::display_result()
+{
+	if (_stack.size() != 1)
+		throw std::invalid_argument("Error: too many operands");
+	std::cout << _stack.top() << std::endl;
 }
--- module_09/ex01/main.cpp
+++ module_09/ex01/main.cpp
@@ -1,91 +1,23 @@
 #include "RPN.hpp"
 
-typedef	std::stack<int>	t_stack;
-
-bool	is_operation(char chr)
-{
-	return (chr == '+' || chr == '-' || chr == '*' || chr == '/');
-}
-
-void	do_operation(t_stack &stack, int a, int b, char op)
-{
-	switch (op)
-	{
-		case '+':
-			stack.push(a + b);
-			break;
-		case '-':
-			stack.push(a - b);
-			break;
-		case '*':
-			stack.push(a * b);
-			break;
-		case '/':
-			stack.push(a / b);
-			break;
-		default:
-			break;
-	}
-	std::cout << b << " " << op << " " << a << " = " << b + a << std::endl;
-}
-
-
-bool is_str_digit(std::string str)
-{
-	for (size_t i = 0; i < str.size(); i++)
-		if (!isdigit(str[i]))
-			return (false);
-	return (true);
-}
-
 int	main(int argc, char const *argv[])
 {
-	t_stack		stack;
-	std::string	expr;
-	int			a, b;
-	size_t		pos, start;
+	RPN	rpn_calculator;
 
 	if (argc != 2)
 		std::cerr << "Error: Invalid arguments" << std::endl;
 	else
 	{
-		pos = 0;
-		start = 0;
-		expr = argv[1];
-		while ((pos = expr.find(' ', start)) && start != expr.size())
+		try
 		{
-			if (!is_operation(expr.substr(start, pos - start)[0]))
-			{
-				if (!is_str_digit(expr.substr(start, pos - start)))
-				{
-					std::cerr << "Error: non-numeric arguments" << std::endl;
-					return (1);
-				}
-				stack.push(atoi(expr.substr(start, pos - start).c_str()));
-				std::cout << "[" << atoi(expr.substr(start, pos - start).c_str()) << "]" << std::endl;
-			}
-			else
-			{
-				if (stack.size() != 2)
-				{
-					std::cerr << "Error" << std::endl;
-					return (1);
-				}
-				a = stack.top();
-				stack.pop();
-				b = stack.top();
-				stack.pop();
-				do_operation(stack, a, b, expr.substr(start, pos - start)[0]);
-			}
-			start = pos + 1;
-			while (expr[start] == ' ')
-				start++;
-			if (pos == std::string::npos)
-				break;
+			rpn_calculator.eval(argv[1]);
+			rpn_calculator.display_result();
+		}
+		catch (std::exception &err)
+		{
+			std::cerr << err.what() << std::endl;
 		}
 	}
-
-	std::cout << "Stack size: " << stack.size() << "  |  result: " << stack.top() << std::endl;
 
 	return (0);
 }
